4535ccfc36443fe519ed09ad7b4836b97c55efb4
maruLab <1109035+maruLab@users.noreply.github.com>
2018-05-31 20:41:14 +0900

    Insertion sort sample.

---
+++ insertionSort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void insertion_sort(int length, int *data);
+void printArray(int length, int *data);
+
+int main(void) {
+	int data[] = { 4, 51, 31, 8, 20, 16, 37, 34, 3, 11, 7, 13, 21, 27, 1 };
+	int length = sizeof(data) / sizeof(data[0]);
+	printArray(length, data);
+	insertion_sort(length, data);
+}
+void insertion_sort(int length, int *data) {
+	int j;
+	int tmp;
+	for (int i = 1; i < length; i++) {
+		j = i;
+		while (0 < j && data[j] < data[j - 1]) {
+			tmp = data[j];
+			data[j] = data[j - 1];
+			data[j - 1] = tmp;
+			j--;
+		}
+		printArray(length, data);
+	}
+}
+void printArray(int length, int *data) {
+	for (int i = 0; i < length; i++) {
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+}
